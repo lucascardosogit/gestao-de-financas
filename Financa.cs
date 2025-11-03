91307af4a2b6f9439aefcd73379d1bc92482eed9
lucas <lucas@LUCAS>
2025-11-03 20:21:19 -0300

    Adicionar arquivos de projeto.

---
+++ Financa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gestao_de_financas
+{
+    public abstract class Financa
+    {
+        public int ID { get; }
+        public string Nome { get; set; }
+    }
+}
